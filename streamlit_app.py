340c009183b592abb5c4bf19ae1f05f1e36986dd
LahuddinLubis <lahuddinlubis@gmail.com>
2024-08-04 07:50:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+import prediksi
 
-st.title('🎈 App Name')
+st.set_page_config(
+    page_title="Prediksi Kredit Menggunakan ANN",
+)
 
-st.write('Hello world!')
+def main():
+    with st.sidebar:
+        app = option_menu(
+            menu_title='Main Menu',
+            options=['Home', 'Prediksi'],
+            icons=['house-fill', 'bi-cash'],
+            menu_icon='chat-text-fill',
+            default_index=0,
+            styles={
+                "container": {"padding": "5!important"},
+                "icon": {"color": "white", "font-size": "18px"},
+                "nav-link": {"color": "white", "font-size": "14px", "text-align": "left", "margin": "0px"},
+                "nav-link-selected": {"background-color": "#02ab21"},
+            }
+        )
+
+    if app == "Prediksi":
+        prediksi.app()
+
+if __name__ == "__main__":
+    main()
